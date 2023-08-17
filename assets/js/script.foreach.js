92128dc2d0c5822667fcfc6e4f59314be7f8be47
sosofria <sophiamarcondes.564@gmail.com>
2023-08-17 10:50:17 -0300

    feat: create createCategory

--- assets/js/script.foreach.js
+++ assets/js/script.foreach.js
@@ -10,8 +10,31 @@ class Category{ //possui varios produtos
 class Product{
     constructor(id, name, price, category){
         this.id = id;
-        this.name = id;
+        this.name = name;
         this.price = price;
         this.category = category;
     }
+}
+
+class CategoryService{
+    constructor(){ //nao chama nenhum parametro
+        this.categories = [];
+        this.nextCategoryId = 0;
+    }
+
+    addCategory(name){
+        const id = this.nextCategoryId ++;
+        const category = new Category(id, name);
+        this.categories.push(category);
+    }
+}
+
+const categoryList = new CategoryService();
+
+function createCategory(){
+    const categoryName = "doce";
+
+    categoryList.addCategory(categoryName);
+
+    console.log(categoryList.categories);
 }
